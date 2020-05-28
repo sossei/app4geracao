9240805cb0c50ef92e571595e0b143e7e5e9d1cd
Vinicius Sossei <viniciussossei@MacBook-Pro-de-Vinicius.local>
2020-05-28 02:02:50 -0300

    Animação

--- lib/control/nav/nav.dart
+++ lib/control/nav/nav.dart
@@ -3,17 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 
 Future push(BuildContext context, Widget page) {
-  return Navigator.push(
-      context,
-      PageTransition(
-          type: PageTransitionType.rightToLeftWithFade, child: page));
+  return Navigator.push(context, AnimRoute(page: page));
 }
 
 pushReplacment(BuildContext context, Widget page) {
-  Navigator.pushReplacement(
-      context,
-      PageTransition(
-          type: PageTransitionType.rightToLeftWithFade, child: page));
+  Navigator.pushReplacement(context, AnimRoute(page: page));
 }
 
 pop(BuildContext context) {
@@ -22,8 +16,41 @@ pop(BuildContext context) {
 
 returnSplash(BuildContext context) {
   Navigator.pushAndRemoveUntil(
-      context,
-      PageTransition(
-          type: PageTransitionType.leftToRightWithFade, child: SplashScreen()),
-      ModalRoute.withName('/'));
+      context, AnimRoute(page: SplashScreen()), ModalRoute.withName('/'));
+}
+
+class AnimRoute extends PageRouteBuilder {
+  final Widget page;
+  AnimRoute({this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionDuration: Duration(milliseconds: 400),
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1, 0.0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: FadeTransition(
+              opacity: animation,
+              child: SlideTransition(
+                position: Tween<Offset>(
+                  begin: Offset.zero,
+                  end: const Offset(-1.0, 0.0),
+                ).animate(secondaryAnimation),
+                child: child,
+              ),
+            ),
+          ),
+        );
 }
